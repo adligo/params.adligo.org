3efb099c1a1c3914a7f72d44f93242729e85e259
scott <scott@adligo.com>
2009-03-07 17:34:21 +0000

    updated for I_InstaceFactory removal replace with I_Factory

--- src/org/adligo/models/params/client/ClassForNameMap.java
+++ src/org/adligo/models/params/client/ClassForNameMap.java
@@ -1,11 +1,12 @@
 package org.adligo.models.params.client;
 
 import org.adligo.i.util.client.ClassUtils;
-import org.adligo.i.util.client.I_InstanceFactory;
+import org.adligo.i.util.client.I_Factory;
 
-public class ClassForNameMap implements I_InstanceFactory {
+public class ClassForNameMap implements I_Factory {
 
-	public Object createInstance(String clazz) {
+	public Object createNew(Object p) {
+		String clazz = (String) p;
 		if (ClassUtils.getClassName(Params.class).equals(clazz)) {
 			return new Params();
 		}
@@ -17,4 +18,5 @@ public class ClassForNameMap implements I_InstanceFactory {
 		}
 		throw new RuntimeException("Couldn't create class " + clazz);
 	}
+
 }
