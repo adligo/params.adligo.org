81cc04e1d324aad6274b28b824a1fbcb9bd18815
scott <scott@adligo.com>
2012-05-16 04:49:22 +0000

    new class

---
+++ src/org/adligo/models/params/client/ParamsFactory.java
@@ -0,0 +1,32 @@
+package org.adligo.models.params.client;
+
+public class ParamsFactory {
+	
+	public static Params defaultById(long id) {
+		Params params = new Params();
+		params.addParam("default");
+		Params whereParams = new Params();
+		params.addParam("where", whereParams);
+		whereParams.addParam("id", SqlOperators.EQUALS, id);
+		return params;
+	}
+	
+	public static Params defaultById(int id) {
+		Params params = new Params();
+		params.addParam("default");
+		Params whereParams = new Params();
+		params.addParam("where", whereParams);
+		whereParams.addParam("id", SqlOperators.EQUALS, id);
+		return params;
+	}
+	
+	public static Params defaultById(String id) {
+		Params params = new Params();
+		params.addParam("default");
+		Params whereParams = new Params();
+		params.addParam("where", whereParams);
+		whereParams.addParam("id", SqlOperators.EQUALS, id);
+		return params;
+	}
+	
+}
