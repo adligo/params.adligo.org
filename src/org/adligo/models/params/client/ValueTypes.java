f63378781fec6370946d651051cbaf275caf9ccf
scott <scott@adligo.com>
2014-02-03 20:55:28 -0600

    for xmlt

--- src/org/adligo/models/params/client/ValueTypes.java
+++ src/org/adligo/models/params/client/ValueTypes.java
@@ -5,15 +5,28 @@ package org.adligo.models.params.client;
  *
  */
 public class ValueTypes {
-	public static final ValueType STRING = new ValueType((short) 0, "String");
-	public static final ValueType LONG = new ValueType((short) 1, "Long");
-	public static final ValueType INTEGER = new ValueType((short) 2, "Integer");
-	public static final ValueType SHORT = new ValueType((short) 3, "Short");
-	public static final ValueType DOUBLE = new ValueType((short) 4, "Double");
-	public static final ValueType FLOAT = new ValueType((short) 5, "Float");
-	public static final ValueType DATE = new ValueType((short) 6, "Date");
-	public static final ValueType TIMESTAMP = new ValueType((short) 7, "Timestamp");
-	public static final ValueType BOOLEAN = new ValueType((short) 8, "Boolean");
-	public static final ValueType BIG_DECIMAL = new ValueType((short) 9, "BigDecimal");
-	public static final ValueType BIG_INTEGER = new ValueType((short) 10, "BigInteger");
+	public static final short STRING_ID = (short) 0;
+	public static final short LONG_ID  = (short)1;
+	public static final short INTEGER_ID  = (short) 2;
+	public static final short SHORT_ID  = (short) 3;
+	public static final short DOUBLE_ID  = (short) 4;
+	public static final short FLOAT_ID  = (short) 5;
+	public static final short DATE_ID  = (short) 6;
+	public static final short TIMESTAMP_ID  = (short) 7;
+	public static final short BOOLEAN_ID  = (short) 8;
+	public static final short BIG_DECIMAL_ID  = (short) 9;
+	public static final short BIG_INTEGER_ID  = (short) 10;
+	
+	public static final ValueType STRING = new ValueType(STRING_ID, "String");
+	
+	public static final ValueType LONG = new ValueType(LONG_ID,"Long");
+	public static final ValueType INTEGER = new ValueType(INTEGER_ID,"Integer");
+	public static final ValueType SHORT = new ValueType(SHORT_ID,"Short");
+	public static final ValueType DOUBLE = new ValueType(DOUBLE_ID, "Double");
+	public static final ValueType FLOAT = new ValueType(FLOAT_ID, "Float");
+	public static final ValueType DATE = new ValueType(DATE_ID,"Date");
+	public static final ValueType TIMESTAMP = new ValueType(TIMESTAMP_ID,"Timestamp");
+	public static final ValueType BOOLEAN = new ValueType(BOOLEAN_ID,"Boolean");
+	public static final ValueType BIG_DECIMAL = new ValueType(BIG_DECIMAL_ID, "BigDecimal");
+	public static final ValueType BIG_INTEGER = new ValueType(BIG_INTEGER_ID,"BigInteger");
 }
